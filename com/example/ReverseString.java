7725a50ae5fa73d5aec606c04a739ffa96ca2904
Kevin <92882620+Credible-Zebra@users.noreply.github.com>
2024-04-30 08:25:54 -0400

    Initial commit

---
+++ com/example/ReverseString.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Kevin Csiffary
+ * @version 1.0
+ * @since 2024-04-30
+ */
+
+// ReverseString class
+public final class ReverseString {
+
+    /** Private constructor to prevent instantiation. */
+    private ReverseString() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
